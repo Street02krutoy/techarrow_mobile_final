8e05de506bd924d410b7a99adb2ed8d204a53c73
srite <masaanekdot@gmail.com>
2025-04-10 04:53:36 +0300

    stfu

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,5 +1,30 @@
+import 'dart:async';
+
+import 'package:chopper/chopper.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:techarrow_mobile_final/auth/keycloak.dart';
 import 'package:techarrow_mobile_final/swagger_generated_api/client_index.dart';
 
 class ApiService {
-  final api = Swagger.create(baseUrl: Uri.parse('http://192.168.168.29:5252'));
+  final api = Swagger.create(
+      baseUrl: Uri.parse('http://192.168.168.29:5252'),
+      interceptors: [AuthInterceptor()]);
+}
+
+class AuthInterceptor implements HeadersInterceptor {
+  AuthInterceptor();
+
+  @override
+  Map<String, String> get headers => {'Authorization': "Bearer token"};
+
+  @override
+  FutureOr<Response<BodyType>> intercept<BodyType>(
+      Chain<BodyType> chain) async {
+    if (await Keycloak().isTokenExpired()) {
+      await Keycloak().refreshToken();
+    }
+    chain.request.headers['Authorization'] =
+        'Bearer ${await FlutterSecureStorage().read(key: "access_token")}';
+    return chain.proceed(chain.request);
+  }
 }
--- lib/auth/keycloak.dart
+++ lib/auth/keycloak.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
 import 'package:openid_client/openid_client_io.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -45,6 +48,29 @@ class Keycloak {
     await storage.write(key: "user_id", value: userInfo["sub"]);
     await storage.write(key: "username", value: userInfo["preferred_username"]);
     return userInfo;
+  }
+
+  Future<void> refreshToken() async {
+    final refreshToken = await storage.read(key: "refresh_token");
+    if (refreshToken == null) return;
+
+    final response = await http.post(
+      Uri.parse("$baseUrl/realms/$realm/protocol/openid-connect/token"),
+      headers: {"Content-Type": "application/x-www-form-urlencoded"},
+      body: {
+        "grant_type": "refresh_token",
+        "client_id": clientId,
+        "refresh_token": refreshToken
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      await storage.write(key: "access_token", value: data["access_token"]);
+      await storage.write(key: "refresh_token", value: data["refresh_token"]);
+      await storage.write(
+          key: "expires_in", value: data["expires_in"].toString());
+    }
   }
 
   Future<bool> isTokenExpired() async {
